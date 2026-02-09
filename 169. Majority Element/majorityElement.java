35776139213e7efb9e8c5ab76fb0800ec9a41898
Nandini pharasi <85411441+NandiniPharasi@users.noreply.github.com>
2026-02-10 00:51:42 +0530

    Implement majority element algorithm in Java

---
+++ 169. Majority Element/majorityElement.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int majorityElement(int[] nums) {//n/2
+    int count=0;
+    int element=0;
+
+    for(int i=0;i<nums.length;i++){
+        if(count==0){
+             element=nums[i];
+            
+        } 
+
+        if(nums[i]==element){
+            count++;
+        } else
+         count--;
+
+    }
+    return element;
+        
+    }
+}
