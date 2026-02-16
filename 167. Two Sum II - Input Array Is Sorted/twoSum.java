084ccfc0ddaf71e8b15c59b074fbfd3728524b94
Nandini pharasi <85411441+NandiniPharasi@users.noreply.github.com>
2026-02-16 22:56:11 +0530

    Implement twoSum method for sorted input array

---
+++ 167. Two Sum II - Input Array Is Sorted/twoSum.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int [] ans = new int[2];
+        int left=0;
+        int right=numbers.length-1;
+        int sum=0;
+        while (left<right){
+            sum =numbers[left]+numbers[right];
+
+            if(sum==target){
+               return new int[] {left+1, right+1};
+            }
+            else if(sum>target){
+                right--;
+                
+
+            } else if (sum< target){
+                left++;
+               
+            } 
+        }
+        return new int[]{-1,-1};
+    }
+}
