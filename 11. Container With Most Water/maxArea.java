329258af2b6c8a5862487817e738586118472770
Nandini pharasi <85411441+NandiniPharasi@users.noreply.github.com>
2026-02-16 23:02:32 +0530

    Implement maxArea method for container problem

---
+++ 11. Container With Most Water/maxArea.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxArea=0;
+        int left=0;
+        int right=height.length-1;
+        
+        while(left<right){
+            int gap=right-left;
+            int ht= Math.min(height[left], height[right]);
+            int area=gap*ht;
+
+            maxArea=Math.max(maxArea, area);
+
+            if(height[left]<height[right]){
+                left++;
+            } else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
