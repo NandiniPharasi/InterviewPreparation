4b47d72a9d386b194972fb451658a52618ecb998
Nandini pharasi <85411441+NandiniPharasi@users.noreply.github.com>
2026-02-17 22:34:27 +0530

    Add cycle detection method for linked list
    Implement Floyd's Cycle Detection algorithm to identify the start of a cycle in a linked list.

---
+++ 142. Linked List Cycle II/detectCycle.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+
+        ListNode slow=head;//initialise (need to move 2 step)
+        ListNode fast=head; //ini (need to move 2 step)
+
+
+        while(fast!=null && fast.next!=null){ // to check if fast has a next value case 2 line 14
+            slow=slow.next; //since value is there we move it to next point
+            fast=fast.next.next;//since value is there we move it to next next point
+             if (slow==fast){
+                slow=head;
+
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+
+
+                return slow; //if at any point slow= fast that mean it's cyclic
+             }
+
+        }
+        return null;
+        
+    
+    }
+}
