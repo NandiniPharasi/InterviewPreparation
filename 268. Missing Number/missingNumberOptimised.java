db8d6c039fcbbdf4872c3e04aafbbed0d37cdffa
Nandini pharasi <85411441+NandiniPharasi@users.noreply.github.com>
2026-03-09 00:49:52 +0530

    Implement missing number finder in Java

---
+++ 268. Missing Number/missingNumberOptimised.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        //If nums contains [0, 1, 2, 3, 5] then we calculate 1+2+3+4+5 then subtract 1+2+3+5 which would give us 4.
+        int total=n*(n+1)/2;
+
+        for(int a: nums){
+            sum+=a;
+        }
+
+        return total-sum;
+    
+    }
+}
