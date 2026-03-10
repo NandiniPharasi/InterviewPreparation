0edc2bae8dc798a68ec9136e2d549bfb678c2b76
Nandini pharasi <85411441+NandiniPharasi@users.noreply.github.com>
2026-03-10 20:30:18 +0530

    Update firstMissingPositiveOptimised.java

--- 41. First Missing Positive/firstMissingPositiveOptimised.java
+++ 41. First Missing Positive/firstMissingPositiveOptimised.java
@@ -1,14 +1,22 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-           int n=nums.length;
-        int sum=0;
-        //If nums contains [0, 1, 2, 3, 5] then we calculate 1+2+3+4+5 then subtract 1+2+3+5 which would give us 4.
-        int total=n*(n+1)/2;
-
-        for(int a: nums){
-            sum+=a;
+        Set<Integer> hs= new TreeSet<>();
+        for(int n: nums){
+            if(n>0){
+                hs.add(n);
+            }
         }
 
-        return total-sum;
+
+        for (int i=1; i<=hs.size(); i++){
+            if(hs.contains(i)){
+
+            }
+            else{
+                return i;
+            }
+        }
+        return hs.size()+1;
+        
     }
 }
