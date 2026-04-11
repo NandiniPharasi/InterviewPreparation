6d156c102db8a29d322f9b1208b1fd470f70f6ae
Nandini pharasi <85411441+NandiniPharasi@users.noreply.github.com>
2026-04-11 23:04:31 +0530

    Add hints for minimum distance calculation
    Added hints for optimizing the distance calculation between three equal elements.

--- 3740. Minimum Distance Between Three Equal Elements I/minimumDistance(self.py
+++ 3740. Minimum Distance Between Three Equal Elements I/minimumDistance(self.py
@@ -11,4 +11,12 @@ class Solution:
                     for k in range(j+1, n):
                         if nums[j] == nums[k]:
                             ans = min(ans, 2*(k-i))
+                            '''
+                            Hint 1
+The distance formula abs(i - j) + abs(j - k) + abs(k - i) simplifies to 2 * (max(i, j, k) - min(i, j, k)).
+Hint 2
+Group the indices for each unique number. For a number to form a good tuple, it must appear at least 3 times.
+Hint 3
+For each number that appears at least 3 times, we want to find three of its indices p < q < r that minimize r - p. This is achieved by considering every three consecutive indices in the sorted list of indices.
+                            '''
         return -1 if ans == float('inf') else ans
